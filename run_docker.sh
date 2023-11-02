fbfea29e8e69486840725ee20209379ce3f77b1c
Maximilian Rapp <maximilian.rapp@tum.de>
2023-11-02 13:13:26 +0800

    automated local running

--- run_docker.sh
+++ run_docker.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+echo "Building the image via docker-compose"
 docker-compose build
+echo "tagging backend-app to backend"
 docker tag backend-app backend
+echo "running the server with exposed port 80 on localhost"
 docker run -d -p 80:80 backend
+echo "Server should return: Hello World"
 curl localhost:80
