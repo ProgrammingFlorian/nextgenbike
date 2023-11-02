1a724d848ff3a674b612d277ce936f08f02ac594
Maximilian Rapp <maximilian.rapp@tum.de>
2023-11-02 15:24:58 +0800

    New db path

--- backend/server/app/config.py
+++ backend/server/app/config.py
@@ -6,5 +6,5 @@ basedir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
 class Config(object):
     # ...
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-                              'sqlite:///' + os.path.join(basedir, '/etc/app/app.db')
+                              'sqlite:///' + os.path.join(basedir, '/etc/data_db/app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
