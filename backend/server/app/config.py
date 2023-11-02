f6c62289cbe02c494d1868e95a847ec1be5b0313
Maximilian Rapp <maximilian.rapp@tum.de>
2023-11-02 17:09:51 +0800

    Change to Postgres database

--- backend/server/app/config.py
+++ backend/server/app/config.py
@@ -5,5 +5,5 @@ from pathlib import Path
 
 class Config(object):
     # ...
-    SQLALCHEMY_DATABASE_URI = "postgresql://docker:s3cr3t@localhost:12345/data_db"
+    SQLALCHEMY_DATABASE_URI = "postgresql://docker:s3cr3t@postgres:5432/data_db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
--- run_docker.sh
+++ run_docker.sh
@@ -4,6 +4,4 @@ docker-compose build
 echo "tagging backend-app to backend"
 docker tag backend-app backend
 echo "running the server with exposed port 80 on localhost"
-docker run -d -p 80:80 backend
-echo "Server should return: Hello World"
-curl localhost:80
+docker run -d -p 80:80 backend -it --mount type=bind,src="$(pwd)"/backend/server/etc/data_db,target=/app/server/etc/data_db
