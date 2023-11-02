86d6652fbb4e89eefcd0628ef2ab9fc48df868ff
Florian <ge95kek@mytum.de>
2023-11-02 16:45:17 +0800

    changed sqlalchemy to postgres

--- backend/server/app/config.py
+++ backend/server/app/config.py
@@ -3,14 +3,7 @@ import os
 import os
 from pathlib import Path
 
-dir_base = os.path.abspath(os.path.join(os.path.dirname(__file__)))
-dir_parent = os.path.join(dir_base, os.pardir)
-dir_database = os.path.join(dir_parent, 'etc/data_db/')
-
-Path(dir_database).mkdir(parents=True, exist_ok=True)
-
 class Config(object):
     # ...
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-                              'sqlite:///' + os.path.join(dir_database, 'app.db')
+    SQLALCHEMY_DATABASE_URI = "postgresql://docker:s3cr3t@localhost:12345/data_db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
