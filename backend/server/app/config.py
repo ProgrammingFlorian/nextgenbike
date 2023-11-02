caf63a3fe9d0548c8bb6302cb6f958bcc864c967
Florian <ge95kek@mytum.de>
2023-11-02 15:36:28 +0800

    correct database path

--- backend/server/app/config.py
+++ backend/server/app/config.py
@@ -1,15 +1,16 @@
 import os
 
 import os
+from pathlib import Path
 
 dir_base = os.path.abspath(os.path.join(os.path.dirname(__file__)))
-dir_parent = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
-dir_database = os.path.join(dir_parent, 'etc/data_db/app.db')
+dir_parent = os.path.join(dir_base, os.pardir)
+dir_database = os.path.join(dir_parent, 'etc/data_db/')
 
-print(dir_database)
+Path(dir_database).mkdir(parents=True, exist_ok=True)
 
 class Config(object):
     # ...
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-                              'sqlite:///' + dir_database
+                              'sqlite:///' + os.path.join(dir_database, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
