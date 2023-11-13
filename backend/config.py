09f9b8767aa448ca20b1d04a145959901c5e808b
Maximilian Rapp <maximilian.rapp@tum.de>
2023-11-14 00:30:51 +0800

    Better Machine Learning

--- backend/config.py
+++ backend/config.py
@@ -1,7 +1,7 @@
-classes = ['asphalt', 'pavement', 'gravel', 'dirt']
+classes = ['asphalt', 'pavement', 'gravel', 'grass']
 learning_rate = 1e-3
 momentum = 0.9
-num_training_epochs = 30
+num_training_epochs = 50
 batch_size = 10
 n_training_cols = 8
 n_hidden_layers = 64  # TODO maybe increase with more data
